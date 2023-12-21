fa9bb00a4c75167ff2e14713b9d6b1ec9da79081
Théo Poizat <hello@zatteo.com>
2023-12-21 11:41:26 +0100

    fix(backup): Do not display 'Backup already preparing' message
    When flagship app backup throw an error, it is displayed by Photos in
    a red alert. We do not want this for 'Backup already preparing'
    message. We will find a better solution on flagship app side later.

--- src/photos/ducks/backup/helpers/error.js
+++ src/photos/ducks/backup/helpers/error.js
@@ -1,11 +1,22 @@
+const IGNORED_ERRORS = [
+  'Backup already preparing',
+  'Copia de seguridad ya se está preparando',
+  'Sauvegarde déjà en préparation'
+]
+
 export const parseBackupError = ({ message }) => {
   try {
     const parsed = JSON.parse(message)
+
     return {
       message: parsed.message,
       statusCode: parsed.statusCode
     }
   } catch {
+    if (IGNORED_ERRORS.includes(message)) {
+      return null
+    }
+
     return {
       message
     }
