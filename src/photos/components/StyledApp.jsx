b5520c6530508f5bbc1d7ec7e8fe1efaa3e435fe
JF-Cozy <julien.ficheux@cozycloud.cc>
2024-06-06 10:59:21 +0200

    feat: Upgrade packages

--- src/photos/components/StyledApp.jsx
+++ src/photos/components/StyledApp.jsx
@@ -4,7 +4,9 @@ import 'photos/styles/main.styl'
 import 'cozy-sharing/dist/stylesheet.css'
 
 import CozyTheme from 'cozy-ui/transpiled/react/providers/CozyTheme'
+
 const StyledApp = ({ children }) => {
   return <CozyTheme className="u-w-100">{children}</CozyTheme>
 }
+
 export default StyledApp
