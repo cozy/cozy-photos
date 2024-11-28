1c4c7ba8c09604ddfcfa9ad922bae538baed664d
cballevre <celestin.ballevre@protonmail.com>
2024-11-28 12:04:55 +0100

    feat: Use flag debug to activate redux devtools

--- src/targets/browser/index.jsx
+++ src/targets/browser/index.jsx
@@ -1,5 +1,3 @@
-/* global cozy __DEVELOPMENT__ */
-
 import 'cozy-ui/dist/cozy-ui.utils.min.css'
 import 'cozy-ui/transpiled/react/stylesheet.css'
 import 'cozy-bar/dist/stylesheet.css'
@@ -66,7 +64,7 @@ const setupAppContext = memoize(() => {
 
   // Enable Redux dev tools
   const composeEnhancers =
-    (__DEVELOPMENT__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+    (flag('debug') && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
 
   const store = createStore(
     combineReducers({ ...appReducers, cozy: client.reducer() }),
